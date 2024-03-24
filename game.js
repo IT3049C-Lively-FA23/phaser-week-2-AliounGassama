6e755be3d64660fe875359f83e95920f1c518a25
AliounGassama <aliounsarrgassama@gmail.com>
2024-03-24 17:13:40 -0400

    movement and back bone

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
 var config = {
     width: 800,
     height: 600,
-    type: Phaser.AUTO,
     scene: [preload, scene1],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    }
+
 }
 
 var game = new Phaser.Game(config);
--- preload.js
+++ preload.js
@@ -4,8 +4,14 @@ class preload extends Phaser.Scene {
     }
 
     preload(){
+        this.load.image('paddle', 'assets/images/paddle.png');
+        this.load.image('ball', 'assets/images/ball.png');
+        this.load.audio('hitPlayer', 'assets/audios/hit1');
+        this.load.audio('hitCPU', 'assets/audios/hit2');
     }
-
+    
     create(){
+        this.add.text(20, 20, "Loading game...");
+        this.scene.start("playGame");
     }
 }
